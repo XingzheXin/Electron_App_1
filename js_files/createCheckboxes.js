0a31bab8fb1719b1074a7965349f6a8a34e44f43
Xingzhe Xin <xingzhexin@outlook.com>
2019-07-05 14:03:26 +0800

    Loading names from DB now works perfectly

---
+++ js_files/createCheckboxes.js
@@ -0,0 +1,53 @@
+// This file makes query to the database and adds elements to the HTML file
+var class_name = document.title;
+retrieveFromDB(class_name);
+
+function retrieveFromDB(classname) {
+    var mysql = require('mysql');
+    var con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "Hbar^2/2m",
+        database: "walkerenglishdb"
+    });
+
+    con.connect(function (err) {
+        if (err) {
+            //window.location.replace("../html/404.html")
+            throw err;
+        }
+    });
+    
+    var str = "SELECT * FROM students WHERE class=" + "'" + classname + "'";
+    con.query(str, function (err, result, fields) {
+        if (err) throw err;
+        for (var i = 0; i < result.length; i++) {
+            var name = result[i].last_name + result[i].first_name;
+            addCheckbox(name);
+        }
+        con.end();
+    });
+}
+
+
+function addCheckbox(studentname) {
+    // Declare relative variables
+    var student_list_div = document.getElementById('studentlist');
+    var cb_container = document.createElement('label');
+    var input = document.createElement('input');
+    var span = document.createElement('span');
+
+    // Set attributes accordingly
+    cb_container.className = "cb_container";
+    cb_container.innerHTML = studentname;
+    input.type = "checkbox";
+    //input.setAttribute("checked", "checked");
+    span.className = "checkmark";
+
+    cb_container.appendChild(input);
+    cb_container.appendChild(span);
+    console.log(cb_container);
+    console.log(student_list_div);
+    student_list_div.appendChild(cb_container);
+    console.log("Inserted checkbox label into the list");
+}
--- main.js
+++ main.js
@@ -12,6 +12,12 @@ const mainMenuTemplate = [
                 label: 'Select Journal File',
             },
             {
+                label: 'Test Insert Into DB',
+                click() {
+                    insertDb();
+                }
+            },
+            {
                 label: 'Choose Class',
                 submenu: [
                     {
@@ -151,10 +157,13 @@ function createMainWindow() {
             nodeIntegration: true
         },
         minHeight: 600,
-        minWidth: 800
+        minWidth: 800,
+        backgroundColor: '#373947',
+        
     })
     mainWindow.setMenu(mainWindowMenuBar);
     mainWindow.loadFile('./html/index.html');
+    mainWindow.webContents.openDevTools();
     mainWindow.on('closed', () => {
         mainWindow = null;
     })
@@ -182,6 +191,7 @@ function createSelectJournalFileWindow() {
 
     mainWindow.setMenu(childWindowMenuBar);
     mainWindow.loadFile('./html/addWindow.html');
+    mainWindow.webContents.openDevTools();
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
@@ -213,25 +223,64 @@ function createDongFengLuZhouSanZhouLiuBanWindow() {
     });
     mainWindow.setMenu(childWindowMenuBar);
     mainWindow.loadFile('./html/东风路周三周六班.html');
+    mainWindow.webContents.openDevTools();
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
 }
 
-//function createDongFengLuXinErYiBanWindow() {
-//    mainWindow = new BrowserWindow({
-//        width: 800,
-//        height: 600,
-//        webPreferences: {
-//            nodeIntegration: true
-//        },
-//        minHeight: 600,
-//        minWidth: 800
-//    });
-//    mainMenu.setMenu(childWindowMenuBar);
-//    mainWindow.loadFile('东风路新二（1）班.html');
-//    mainWindow.setApplicationMenu(null);
-//    mainWindow.on('closed', () => {
-//        mainWindow = null;
-//    });
-//}
+function createDongFengLuXinErYiBanWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        minHeight: 600,
+        minWidth: 800
+    });
+    mainWindow.setMenu(childWindowMenuBar);
+    mainWindow.loadFile('./html/东风路新二（1）班.html');
+    mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+function insertDb() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        minHeight: 600,
+        minWidth: 800
+    });
+    mainWindow.setMenu(childWindowMenuBar);
+    mainWindow.loadFile('./html/insert.html');
+
+    let mysql = require('mysql');
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Hbar^2/2m',
+        database: 'walkerenglishdb',
+    });
+
+    let sql = `INSERT INTO parents (first_name, last_name)
+               VALUES ("大智", "贺") `;
+
+    let todo = ['Insert one parent into the parent table', false];
+
+    connection.query(sql, todo, (err, results, fields) => {
+        if (err) {
+            return console.error(err.message);
+        }
+    });
+
+    connection.end();
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
